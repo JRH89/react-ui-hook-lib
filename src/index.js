1b09a5e03d752b7125dc6f0398792ddd9ec68903
JRH89 <thegemlins@gmail.com>
2023-08-17 21:10:28 -0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
 import './styles.css' // Import your CSS file
-import Button from './Button'
-import ProgressBar from './ProgressBar'
-import useLocalStorage from './useLocalStorage'
-import StyledInput from './StyledInput'
-import Gallery from './Gallery'
-import GalleryItem from './GalleryItem'
-import { NotificationProvider, useNotification, Notification } from './useNotification'
-
-// Add more imports for other components and hooks if needed
-
-export {
-	Gallery,
-	GalleryItem,
-	Button,
-	ProgressBar,
-	useLocalStorage,
-	StyledInput,
-	NotificationProvider, // Export NotificationProvider
-	useNotification, // Export useNotification
-	Notification, // Export Notification
-	// Export other components and hooks as needed
-}
-
-// Also export each component as default
-export default {
-	Gallery,
-	GalleryItem,
-	Button,
-	ProgressBar,
-	useLocalStorage,
-	StyledInput,
-	NotificationProvider,
-	useNotification,
-	Notification,
-	// Export other components and hooks as needed
-}
+export { default as Gallery } from './Gallery'
+export { default as GalleryItem } from './GalleryItem'
+export { default as Button } from './Button'
+export { default as ProgressBar } from './ProgressBar'
+export { default as useLocalStorage } from './useLocalStorage'
+export { default as StyledInput } from './StyledInput'
+export { NotificationProvider, useNotification, Notification } from './useNotification';
+// Add more exports for other components and hooks if needed
