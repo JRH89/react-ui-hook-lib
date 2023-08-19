9b9d0089143ac6598e79d41e5e22c5d50f2df6c3
JRH89 <thegemlins@gmail.com>
2023-08-18 18:27:22 -0700

    ok

--- src/Toast.js
+++ src/Toast.js
@@ -2,7 +2,6 @@ import React, { useCallback, useEffect } from 'react'
 import './styles.css'
 
 const Toast = ({ toastlist, position, setList }) => {
-
 	const deleteToast = useCallback(id => {
 		const toastListItem = toastlist.filter(e => e.id !== id)
 		setList(toastListItem)
@@ -22,21 +21,19 @@ const Toast = ({ toastlist, position, setList }) => {
 
 	return (
 		<div className={`container ${position}`}>
-			{
-				toastlist.map((toast, i) => (
-					<div
-						key={i}
-						className={`notification toast ${position} ${toast.type}`}
-						style={{ backgroundColor: toast.backgroundColor }}
-					>
-						<button onClick={() => deleteToast(toast.id)}>X</button>
-						<div>
-							<p className="title">{toast.title}</p>
-							<p className="description">{toast.description}</p>
-						</div>
+			{toastlist.map((toast, i) => (
+				<div
+					key={i}
+					className={`notification toast ${toast.type}`}
+					style={{ backgroundColor: toast.backgroundColor }}
+				>
+					<button onClick={() => deleteToast(toast.id)}>X</button>
+					<div>
+						<p className="title">{toast.title}</p>
+						<p className="description">{toast.description}</p>
 					</div>
-				))
-			}
+				</div>
+			))}
 		</div>
 	)
 }
